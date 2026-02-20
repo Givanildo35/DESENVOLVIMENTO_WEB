15e551c3698bdd12a23c13557831f98f6d6a6add
givanildo35 <givasimoes@gmail.com>
2026-02-19 21:41:43 -0300

    aula dia 19

--- minha-spa/src/pages/Home.jsx
+++ minha-spa/src/pages/Home.jsx
@@ -2,14 +2,22 @@ import styles from '../styles/Home.module.css';
 export function Home(){
     return(
         <div className={styles.container}>
-            <div>
-                <h1>Bem-vindo à nossa Single Page Application</h1>
-                <p>Este é um exemplo de renderização via componetes</p>
-            </div>
+            <header className={styles.hero}>
+                <h1 className={styles.title}>Bem-vindo à nossa Single Page Application</h1>
+                <p className={styles.subtitle}>Este é um exemplo de renderização via componetes</p>
+            </header >
+           
+            <section className={styles.features}>
+                <div className={styles.card}>
+                    <h3>Componetes</h3>
+                    <p>Dividindo a interface em partes reultilizaveis.</p>   
+                </div> 
 
-            <div className={styles.content}>
-                <p>Se você esta vendo isso, é porque funcionou</p>
-            </div>
+                <div className={styles.card}>
+                    <h3>CSS Modules</h3>
+                    <p>Estilos que nao vazam para outras paginas</p>
+                </div>
+            </section>
         </div>
 
     );
