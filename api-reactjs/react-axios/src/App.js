d2f0c119ad250406e3ed3ab2b97a3d8068b2936c
givanildo35 <givasimoes@gmail.com>
2026-02-23 21:29:56 -0300

    aula do dia 23/02

--- api-reactjs/react-axios/src/App.js
+++ api-reactjs/react-axios/src/App.js
@@ -1,28 +1,63 @@
 import React, {useEffect, useState} from "react";
 import api from "./services/api";
+import './App.css';
 
 export default function App() {
-  const [user, setUser] = useState(null);
+  const[username, setUsername] = useState('Givanildo35');
+  const[user, setUser] = useState(null);
+  const[repos, setRepos] = useState([]);
+  const[loading, setLoading] = useState(true);
+  const[error, setError]= useState(null);
+
+
 
   // Carrega dados da API
   useEffect(() => {
-    async function loadUser() {
-      try {
-        const response = await api.get('/users/Givanildo35');
-        setUser(response.data);
-      } catch (error) {  // ✅ Corrigido
-        console.error('Error', error);
-      }  
+    async function loadData ( userToSearch = username){
+      try{
+        setLoading(true);
+        setError(null);
+
+        // carrega usuario
+        const userResponse = await api.get(`/users/${userToSearch}`); // Api responde
+        setUser(userResponse.data); //extrai os dados da API
+
+        const reposResponse = await api.get(`/users/${userToSearch}/repos?per_page=6&sort=updated`);
+        setRepos (reposResponse.data);
+
+      } catch(error){
+        console.error('Erro', error);
+        setError(`Usuario ${userToSearch} não encontrado`);
+
+      } finally{
+        setLoading(false);
+      }
+      loadData();
     }
-    loadUser();
   }, []);
 
-  if (!user) return <div>Carregando...</div>;
+  if (loading) {
+    return( 
+      <div className="loadind-container">
+          <div className="spinner"></div>
+          <p>Carregando perfil...</p>
+      </div>
+    );
+  }
 
+  if(error || !user){
+    return(
+      <div className="error-container">
+        <h2>❌{error}</h2>
+        <button> onClick={()=>window.location.reload()}</button>
+
+      </div>
+    )
+  }
   return (
-    <div className="App">  {/* ✅ Corrigido */}
-      <p>Usuario: {user.login}</p>
-      <p>Biografia: {user.bio || 'Sem Biografia'}</p>
+    <div className="app-container">
+      <div className="profile-card"></div>  
+      
     </div>
   );
 }
